117a0d2b3bb6328eafba9116b01149a6dc1f64d8
Nandraj <nabinbhatt62@gmail.com>
2025-03-03 13:16:20 +0530

    remove key

--- dataAdditionTemp/doctorDb.js
+++ dataAdditionTemp/doctorDb.js
@@ -6,7 +6,7 @@ const DoctorJson = require("./doctors.json");
 
 const start = async () => {
   try {
-    await connectDB("mongodb+srv://nabinbhatt62:fWorWdxclnsdaTAu@v0.hfizt.mongodb.net/?retryWrites=true&w=majority&appName=V0");
+    await connectDB("");
     await Doctor.create(DoctorJson);
     console.log("Doctor data POST success");
   } catch (error) {
